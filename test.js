941c1aaa3326b2e046dd2262b81e2ecd93f9dd3f
MrTimcakes <MrTimcakes@gmail.com>
2016-06-11 04:36:43 +0100

    Update tests to exclude Linux as not to fail for travis

--- test.js
+++ test.js
@@ -1,6 +1,10 @@
+if(process.platform=='win32'){
+
 var hide = require('./main.js');
 
 var handle = hide.findWindow('Untitled - Notepad')
 
 hide.hideWindow(handle);
 setTimeout(function(){hide.showWindow(handle);},500);
+
+}
