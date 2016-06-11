781f897755036f9af8ffe9c57a6d80424bcbf998
MrTimcakes <MrTimcakes@gmail.com>
2016-06-11 07:12:56 +0100

    Added Tests For Visible Windows

--- test.js
+++ test.js
@@ -7,4 +7,7 @@ var handle = hide.findWindow('Untitled - Notepad')
 hide.hideWindow(handle);
 setTimeout(function(){hide.showWindow(handle);},500);
 
+
+hide.visableWindows(function(data){console.log(JSON.stringify(data))});
+
 }
